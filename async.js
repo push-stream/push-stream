0b6fa697228fdd5477ebbdf2c745e6bf70b88d03
Dominic Tarr <dominic.tarr@gmail.com>
2018-02-08 23:38:08 +1300

    keep count of inflight ops

--- async.js
+++ async.js
@@ -4,17 +4,23 @@ function AsyncStream (fn) {
   this._fn = fn
   this.buffer = []
   this.paused = true
+  this._inflight = 0
   this.source = this.sink = null
 }
 
 AsyncStream.prototype.write = function (data) {
   var self = this
   this.paused = true
+  this._inflight ++
   this._fn(data, function (err, _data) {
-    if(err) self.sink.end(err)
+    this._inflight--
+    if(err && err !== true) return self.sink.end(err)
     else self.sink.write(_data)
 
-    if(self.paused && !self.sink.paused) self.resume()
+    if(self.paused && !self.sink.paused) {
+      self.paused = false
+      self.resume()
+    }
   })
 }
 
@@ -22,14 +28,16 @@ AsyncStream.prototype.pipe = require('./pipe')
 
 AsyncStream.prototype.resume = function () {
   this.paused = false
-  if(this.ended) this.sink.end(this.ended === true ? null : this.ended)
+  if(this.ended && !this.inflight) this.sink.end(this.ended === true ? null : this.ended)
   else if (this.source) this.source.resume()
 }
 
 AsyncStream.prototype.end = function (err) {
   this.ended = err || true
-  if(!this.paused) this.sink.end(this.ended)
+  if(!this.inflight) this.sink.end(this.ended)
 }
+
+
 
 
 
