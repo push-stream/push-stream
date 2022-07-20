f4d4b53174c7e80d9aa76964bf15617c2aeae67c
Andre Staltz <andre@staltz.com>
2022-07-20 16:54:48 +0300

    modernize codebase

--- test/collect.js
+++ test/collect.js
@@ -8,11 +8,8 @@ var AsyncStream = require('../throughs/async')
 //  console.log(err, ary)
 //}))
 
-new ValuesStream([4,5,6]).pipe(new AsyncStream(function (data, cb) {
-  console.log('async', data)
-  setTimeout(function () {
-    cb(null, data)
-  }, 333)
-})).pipe(new CollectStream(function (err, ary) {
-  console.log('async-end', err, ary)
-}))
+// new ValuesStream([4,5,6]).pipe(new AsyncStream(function (data, cb) {
+//   setTimeout(function () {
+//     cb(null, data)
+//   }, 333)
+// })).pipe(new CollectStream(function (err, ary) { }))
