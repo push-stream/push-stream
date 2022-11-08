e0fba9e31917912a9e26bbd48659deadfd16e2df
Andre Staltz <andre@staltz.com>
2022-11-08 10:27:44 +0200

    make drain accepts null as 1st arg

--- sinks/drain.js
+++ sinks/drain.js
@@ -1,16 +1,25 @@
-
 module.exports = function (each, done) {
-  return {
-    paused: false,
-    write: function (data) {
-      if(each(data) === false) {
-        this.abort()
-      }
-    },
-    end: done,
-    abort: function (err) {
-      this.ended = err || true
-      this.source.abort(err)
-    }
+  return new DrainStream(each, done)
+}
+
+function DrainStream (each, done) {
+  this.paused = false
+  this._each = each
+  this._done = done
+}
+
+DrainStream.prototype.write = function (data) {
+  if (!this._each) return
+  if (this._each(data) === false) {
+    this.abort();
   }
+}
+
+DrainStream.prototype.end = function (err) {
+  if (this._done) this._done(err)
+}
+
+DrainStream.prototype.abort = function (err) {
+  this.ended = err || true
+  this.source.abort(err)
 }
